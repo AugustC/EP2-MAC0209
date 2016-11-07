2c016f1635ad9f7665e5723c9356bbb7d403174a
AugustC <augusto.cms7@gmail.com>
2016-11-07 18:02:09 -0200

    Tempo no eixo x arrumado

--- graficos_voo.py
+++ graficos_voo.py
@@ -4,6 +4,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from datetime import datetime
+from datetime import timedelta
+fmt = '%H:%M:%S:%f'
+
+def fixMicroseconds(timestamp):
+    # Quick hack para arrumar os microsegundos dos dados
+    parts = timestamp.split(':')
+    return ':'.join(
+        parts[:-1] + ['{:03d}'.format(int(parts[-1]))]
+    )
+
+def convert_time(timeseries):
+    initial = datetime.strptime(timeseries[0], fmt)
+    for i in range(len(timeseries)):
+        ti = datetime.strptime(fixMicroseconds(timeseries[i]), fmt)
+        diff = ti - initial
+        timeseries[i] = diff.total_seconds()
+    return timeseries
 
 def grafico_aceleracao(filename, nome):
     dados = pd.read_csv(filename, sep=",")
@@ -11,7 +29,8 @@ def grafico_aceleracao(filename, nome):
     vooY = dados["y"]
     vooZ = dados["z"]
     voo_resultante = (vooX**2 + vooY**2 + vooZ**2)**(1/2)
-    tempo = dados["time"]
+    tempo = dados["time"].values
+    tempo = convert_time(tempo)
     plt.plot(tempo,voo_resultante)
     plt.title(nome)
     plt.ylabel(u"Aceleração Resultante")
